b73cf1df542866c541f1784a99f21b6ec036c9ea
Nathalie Redick <>
2021-10-03 03:45:01 -0400

    connected vision api to webapp

--- app/static/get_labels.py
+++ app/static/get_labels.py
@@ -13,18 +13,14 @@ def get_labels(url):  # func to get labels and confidences from an image
     image = vision.Image()
     image.source.image_uri = url
 
-    # Performs label detection on the image file
+    # Performs label detection on the image file    
     response = client.label_detection(image=image)
-    labels = response.label_annotations
+    labels = response.label_annotations     
 
     if response.error.message:
         return 'Error: Image not found.'
     else:
-        try:  # get best three 
-            return [{label.description : label.score} for label in labels][:3]
-        except:  # or return all if there are less than 3 labels
-            return [{label.description : label.score} for label in labels]
+        d = {k : v for k, v in [[label.description, label.score] for label in labels][:3]}
+        return d
 
-## for testing ## 
-#print(get_labels('https://www.vets4pets.com/siteassets/species/dog/two-dogs-running-with-balls.jpg?w=585&scale=down'))
 
