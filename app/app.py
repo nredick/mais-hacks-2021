e77d5a45757c79993fb33d961ac811e9d60759e8
Nathalie Redick <>
2021-10-02 22:33:36 -0400

    restructure html and add js

--- app/app.py
+++ app/app.py
@@ -1,8 +1,21 @@
+import os
+import sys
+import csv
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from werkzeug.utils import secure_filename
+
+# Some utilites
 import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+#from util import base64_to_pil
+from PIL import Image
+
+from io import BytesIO
+import base64
 
 app = Flask(__name__, template_folder='templates')
+
 #model = pickle.load(open('../model/model.pkl', 'rb'))
 
 @app.route('/', methods=['GET'])
@@ -16,6 +29,37 @@ def about():
 @app.route('/who', methods=['GET'])
 def who():
     return render_template('who.html')
+
+
+
+
+
+
+## image upload 
+
+@app.route('/generate', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = Image.open(BytesIO(base64.b64decode(request.json)))
+        img.show()
+        #img = base64_to_pil(request.json)
+
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 @app.route('/generate', methods=['POST'])
